c3d1dde5656a5b743c6de8a40ba684169451400a
Amirouche <amirouche+dev@hyper.dev>
2019-07-24 18:12:23 +0200

    prelude++

--- prelude.js
+++ prelude.js
@@ -1,6 +1,11 @@
+function pk(x, y, z, t) {
+    console.log("pk", x, y, z, t);
+}
+
 function trampoline(thunk) {
+    console.log('trampoline start', thunk)
     while (thunk && typeof thunk === "function") {
-//        console.log('boing');
+        console.log('boing', thunk);
         thunk = thunk();
     }
     return thunk
