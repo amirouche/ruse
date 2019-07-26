f53a0558c78d54cb8dc99916420cdf30316949a6
Amirouche <amirouche+dev@hyper.dev>
2019-07-26 15:03:46 +0200

    make: check: do not try to run scheme output
    because it requires some primitives to be defined where JavaScript
    will rely on a Foreign Function Interface to implement most
    "primitives".

--- make-check.sh
+++ make-check.sh
@@ -9,9 +9,8 @@ TIMEOUT=15
 
 for file in $(ls $PWD/tests/*.program.scm); do
     echo "* Trying: \"$file\""
-    # check translation into a subset of scheme is still correct
+    # save translation to scheme for reference
     timeout $TIMEOUT scheme --script ruse.scm scheme "$file" > "$file.out.scm"
-    timeout $TIMEOUT scheme --script "$file.out.scm" > "$file.out.scm.txt"
     # check javascript
     timeout $TIMEOUT scheme --script ruse.scm javascript "$file" > "tmp-make-check.js"
     cat prelude.js tmp-make-check.js postlude.js > "$file.out.js"
